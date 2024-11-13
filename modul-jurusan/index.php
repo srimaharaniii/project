dff9082f547b696a464c431c3e1591966312f319
KevinNuriawan <120350473+KevinNuriawan@users.noreply.github.com>
2024-11-13 15:17:57 +0700

    memperbaiki link navbar dan membuat index jurusan

---
+++ modul-jurusan/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Jurusan</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+  
+<?php include_once ('../navbar.php');
+
+?>
+<div class="container">
+    <div clas="row mt-">
+        <div class="col-6 m-auto">
+            <div class="card">
+            <div class="card-header">
+                <h3>Data Jurusan</h3>
+            </div>
+            <div class="card-body">
+                <h4>Selamat Datang Kepada Pengunjung</h4>
+                <p>Kata-kata hari ini "PAHAM"</p>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -7,13 +7,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+          <a class="nav-link active" aria-current="page" href="/project/index.php">Beranda</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Data Mahasiswa</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Data Jurusan</a>
+          <a class="nav-link active" aria-current="page" href="/project/modul-jurusan/index.php">Data Jurusan</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Data Dosen</a>
