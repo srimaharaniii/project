0d80562772bb67986bcaae5a6d6b59d9adba2e70
KevinNuriawan <120350473+KevinNuriawan@users.noreply.github.com>
2024-12-21 10:04:21 +0700

    memmbuat halaman cek login

---
+++ ceklogin.php
@@ -0,0 +1,7 @@
+<?php
+// jika tidak ada $_SESSION dan $_COOKIE maka 
+// alihkan ke halaman login
+if(!isset($_SESSION['sid']) AND !isset($_COOKIE['cid'])){
+    header("location:login.php");
+}
+?>
